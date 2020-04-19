9164cc7637e1aad9e902a24f13d75c447fbcf42f
nana <nykim@nykim.net>
2020-04-19 17:04:50 +0900

    Add a sign on event with js

--- project/homework_1/_src/_js/scrollAnimation.js
+++ project/homework_1/_src/_js/scrollAnimation.js
@@ -1,16 +1,13 @@
 (function () {
-  console.log("SCROLL ANIMATION");
-
-  // 패럴렉스 스크롤 아이템
   const parallaxItems = document.querySelectorAll(".js-parallax");
+  const signOnItem = document.querySelector(".js-sign")
+  const fixedBtn = document.querySelector(".btn-apply")
+  const parallax = [0.2, 0.3, 0.15]
+  let scrollY = 0;
 
   // 패럴렉스 스크롤 이벤트
   const parllaxEvent = function () {
-    //스크롤 값
-    let scrollY = window.scrollY;
-
-    //시차 값 (값이 클수록 더 많이 이동합니다)
-    let parallax = [0.2, 0.3, 0.15]
+    scrollY = window.scrollY;
 
     //요소의 스타일 속성 변경
     parallaxItems.forEach((item, idx) => {
@@ -18,9 +15,33 @@
     })
   };
 
+  // 네온사인 켜지는 이벤트
+  const signOnEvent = function () {
+    scrollY = window.scrollY;
+
+    // 값 체크
+    var windowHeight = window.innerHeight;
+    var itemPosition = signOnItem.getBoundingClientRect().top;
+    console.log("windowHeight", windowHeight);
+    console.log("itemPosition", itemPosition);
+
+    var a = windowHeight;
+    var b = fixedBtn.getBoundingClientRect().height;
+    console.log(b);
+
+
+    if (itemPosition < a - (b * 2.3)) {
+      console.log("!");
+      signOnItem.classList.add("-ani-show");
+    }
+
+
+  };
+
   // 스크롤 애니메이션 핸들러
   const scrollHandler = function () {
     parllaxEvent();
+    signOnEvent();
   };
 
   window.addEventListener("scroll", scrollHandler);
