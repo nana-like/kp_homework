eb2043b2864cb76f2c5e1a0de5eab88c16b4b071
nana <nykim@nykim.net>
2020-04-21 19:39:55 +0900

    Update a component page with input and selectbox

--- project/homework_2/assets/js/Selectbox.js
+++ project/homework_2/assets/js/Selectbox.js
@@ -1,30 +1,95 @@
-var testbox1 = document.querySelector(".testbox");
+// 고유 셀렉트박스 ID 설정용
+let selectID = 0;
 
-var boxes = document.querySelectorAll(".kp-selectbox");
+// 셀렉트박스 데모 영역
+const selectboxGuide = document.querySelector("#selectbox-demos");
 
-function testclick(box) {
+// 셀렉트박스 아이템 생성
+class Selectbox {
+  constructor(param) {
 
-  var testbox = box.children[0] || testbox1;
-  var txt = testbox.options[testbox.selectedIndex].text;
+    selectID++;
 
-  // testbox.nextElementSibling.innerText = txt;
-  testbox.nextElementSibling.dataset.text = txt;
+    const {
+      labels,
+      values = labels,
+      attribute
+    } = param;
 
-  console.dir(txt);
+    // kp-selectbox 생성
+    this.elem = document.createElement("div");
+    this.elem.classList.add("kp-selectbox");
+
+    this.elem.innerHTML += `<select class="kp-select-wrap" name="select${selectID}" id="select${selectID}">
+    </select>`;
+    for (let i = 0; i < values.length; i++) {
+      this.option = document.createElement("OPTION");
+      this.option.value = values[i];
+      this.option.text = labels[i];
+      console.log(this.option);
+      this.elem.children[0].appendChild(this.option);
+    }
+    this.elem.innerHTML += `<div class="kp-selected" data-text=""></div>`;
+
+
+    // 어트리뷰트가 있는 경우
+    if (attribute) {
+      this.elem.children[0].setAttribute(`${attribute}`, true);
+    }
+
+    return this.elem;
+  }
 }
 
+// 셀렉트박스 옵션 설정
+const selectboxOptions = {
+  normal: {
+    labels: ["Option 1", "Option 2", "Option 3"],
+    values: ["opt1", "opt2", "opt3"]
+  },
+  disabled: {
+    labels: ["선택할 수 없는 옵션", "두 번째 옵션", "세 번째 옵션"],
+    values: ["opt1", "opt2", "opt3"],
+    attribute: "disabled",
+  },
+}
 
+// 데모 생성 옵션
+const selectboxDemo = [{
+    id: "selectDemo1",
+    title: "Default",
+    elem: new Selectbox(selectboxOptions.normal)
+  },
+  {
+    id: "selectDemo2",
+    title: "Disabled",
+    desc: "select에 <code>disabled</code> 속성을 추가합니다.",
+    elem: new Selectbox(selectboxOptions.disabled)
+  }
+]
+
+// 셀렉트박스의 라벨을 보여줌 (말줄임표 처리를 위해 data-text에 값을 넣어줌)
+const showLabel = (selectbox) => {
+  var box = selectbox.childNodes[0];
+  var txt = box.options[box.selectedIndex].text;
+  box.nextElementSibling.dataset.text = txt;
+}
+
+// 셀렉트 박스 - 데모 컨테이너 생성
+for (let i = 0; i < selectboxDemo.length; i++) {
+  selectboxGuide.appendChild(new DemoContainer(selectboxDemo[i]));
+}
+
+// 모든 셀렉트 박스 선택
+const boxes = document.querySelectorAll(".kp-selectbox");
+
+// 라벨 삽입
 boxes.forEach((item) => {
   item.addEventListener("change", function () {
-    testclick(item);
-    console.log("!");
+    showLabel(item);
   });
 })
 
-window.addEventListener("load", function () {
-  for (let i = 0; i < boxes.length; i++) {
-    testclick(boxes[i]);
-  }
-});
-// window.addEventListener("load", testclick);
-// testbox.addEventListener("change", testclick);
+for (let i = 0; i < boxes.length; i++) {
+  showLabel(boxes[i]);
+}
--- project/homework_2/assets/js/TextInput.js
+++ project/homework_2/assets/js/TextInput.js
@@ -1,193 +1,168 @@
+// 고유 인풋 ID 설정용
 let inputID = 0;
 
-function TextInput(param) {
-
-  inputID++;
-  const {
-    label,
-    placeholder = "내용을 입력해주세요",
-    value = "",
-    state,
-    type,
-    hasError
-  } = param;
-
-  this.elem = document.createElement("div");
-  this.elem.classList.add("kp-input-item");
-
-  if (label) {
-    this.elem.innerHTML += `<label for="test${inputID}">${label}</label>`;
-  }
-
-  this.elem.innerHTML += `<input id="test${inputID}" type="text" placeholder="${placeholder}" value="${value}" /><div class="explain"></div>
-    `;
-
-  if (state) {
-    this.elem.classList.add(`state-${state}`);
-  }
-  if (type) {
-    this.elem.children[1].setAttribute(`${type}`, true);
-  }
-  if (hasError) {
-    this.elem.classList.add(`state-error`);
-    this.elem.children[2].innerHTML = hasError;
-  }
-
-  return this.elem;
-}
-
-var normal = {
-  label: "일반 폼"
-}
-
-var noLabel = {
-  placeholder: "라벨 없이 인풋 필드만 사용할 수도 있습니다"
-}
-
-var disabled = {
-  label: "사용 불가 폼",
-  type: "disabled",
-  placeholder: "disabled 속성이 붙으면 폼을 사용할 수 없습니다"
-}
-
-var readonly = {
-  label: "읽기 전용 폼",
-  type: "readonly",
-  value: "readonly 속성이 붙은 읽기 전용 폼입니다"
-}
-
-var required = {
-  label: "필수 입력 폼",
-  state: "required",
-  value: "필수 입력 폼은 라벨 오른쪽에 별이 붙습니다"
-}
-
-var requiredError = {
-  label: "필수 입력 + 에러",
-  state: "required",
-  placeholder: "에러는 어떤 폼에든 쓸 수 있습니다",
-  hasError: "에러가 있으면 이렇게 색상이 바뀌어요!"
-}
-
-
-const showCode = (target) => {
-  const html = target.elem.outerHTML;
-  var a = html.replace(/</gi, "&lt;");
-  console.dir(a);
-  const codeElem =
-    this.elem = document.createElement("pre");
-
-  codeElem.innerHTML = html.toString();
-  var b = `<code>${a}</code>`
-  console.dir(b);
-  document.body.appendChild(codeElem);
-  codeElem.innerHTML = b;
-}
-
-
-// new TextInput(normal);
-// new TextInput(disabled);
-// new TextInput(readonly);
-// var r = new TextInput(required);
-// showCode(r);
-// var a = new TextInput(requiredError);
-// showCode(a);
-// var z = new TextInput(noLabel);
-// showCode(z);
-
-
-
+// 텍스트 인풋 데모 영역
 const textInputGuide = document.querySelector("#textinput-demos");
 
+// 마크업 코드 생성
 const generateCode = (target) => {
   const html = target.outerHTML;
-  console.log(html)
-  const result = tidy_html5(html, options);
-  console.log(result)
+  const result = tidy_html5(html, tidyOptions);
   const code = result.replace(/</gi, "&lt;");
-
   return code;
 }
 
-
-var options = {
-  // "indent": "auto",
-  "indent": true,
-  "indent-spaces": 2,
-  "indent-attributes": true,
-  "wrap": 80,
-  "markup": true,
-  "output-xml": false,
-  // "numeric-entities": true,
-  // "quote-marks": true,
-  // "quote-nbsp": false,
-  "show-body-only": true,
-  // "quote-ampersand": false,
-  // "break-before-br": false,
-  // "uppercase-tags": false,
-  // "uppercase-attributes": false,
-  // "drop-font-tags": true,
-  // "tidy-mark": false
-}
-
-function DemoContainer(param) {
-
-  const {
-    id,
-    title,
-    elem
-  } = param;
-
-
-  const code = generateCode(elem);
-
-  const demoCodeTemplate = `
-<div class="demo__code">
-  <div class="code-toggle">
+// 데모 아이템 생성
+class DemoContainer {
+  constructor(param) {
+    const {
+      id,
+      title,
+      elem,
+      desc
+    } = param;
+    const code = generateCode(elem);
+    const demoCodeTemplate = `
+    <div class="demo__code">
+    <div class="code-toggle">
     <label for="${id}">
-      마크업 확인
+    마크업 확인
     </label>
     <input id="${id}" type="checkbox" />
     <div class="code-contents">
-      <pre><code>${code}</code></pre>
+    <pre><code>${code}</code></pre>
     </div>
-  </div>
-</div>
-`;
-
-
-  this.demo = document.createElement("div");
-  this.demo.classList.add("demo");
-  this.demo.innerHTML += `<h3>${title}</h3>`;
-
-  this.demoUI = document.createElement("div");
-  this.demoUI.classList.add("demo__ui");
-  this.demoUI.appendChild(elem);
-
-
-  this.demo.appendChild(this.demoUI);
-  this.demo.innerHTML += demoCodeTemplate;
-
-
-  return this.demo;
-
-}
-
-const obj = {
-  id: "demo123",
-  title: "Default",
-  elem: new TextInput(normal)
+    </div>
+    </div>
+    `;
+    this.demo = document.createElement("div");
+    this.demo.classList.add("demo");
+    this.demo.innerHTML += `<h3>${title}</h3>`;
+    if (desc) {
+      this.demo.innerHTML += `<p class="desc">${desc}</p>`;
+    }
+    this.demoUI = document.createElement("div");
+    this.demoUI.classList.add("demo__ui");
+    this.demoUI.appendChild(elem);
+    this.demo.appendChild(this.demoUI);
+    this.demo.innerHTML += demoCodeTemplate;
+    return this.demo;
+  }
 }
 
 
-const obj2 = {
-  id: "demo999",
-  title: "Reqruied",
-  elem: new TextInput(required)
+// 텍스트 인풋 아이템 생성
+class TextInput {
+  constructor(param) {
+
+    inputID++;
+
+    const {
+      label,
+      placeholder = "내용을 입력해주세요",
+      value = "",
+      state,
+      attribute,
+      hasError
+    } = param;
+
+    // kp-input-item 생성
+    this.elem = document.createElement("div");
+    this.elem.classList.add("kp-input-item");
+
+    // 라벨이 있는 경우
+    if (label) {
+      this.elem.innerHTML += `<label for="test${inputID}">${label}</label>`;
+    }
+
+    this.elem.innerHTML += `<input id="test${inputID}" type="text" placeholder="${placeholder}" value="${value}" /><div class="explain"></div>
+    `;
+
+    // 상태가 설정된 경우
+    if (state) {
+      this.elem.classList.add(`state-${state}`);
+    }
+
+    // 어트리뷰트가 있는 경우
+    if (attribute) {
+      this.elem.children[1].setAttribute(`${attribute}`, true);
+    }
+
+    // 에러인 경우
+    if (hasError) {
+      this.elem.classList.add(`state-error`);
+      this.elem.children[2].innerHTML = hasError;
+    }
+
+    return this.elem;
+  }
 }
 
 
+// 텍스트 인풋 옵션 설정
+const textInputOptions = {
+  normal: {
+    label: "일반 폼"
+  },
+  noLabel: {
+    placeholder: "라벨 없이 인풋 필드만 사용할 수도 있습니다"
+  },
+  disabled: {
+    label: "사용 불가 폼",
+    attribute: "disabled",
+    placeholder: "disabled 속성이 붙으면 폼을 사용할 수 없습니다"
+  },
+  readonly: {
+    label: "읽기 전용 폼",
+    attribute: "readonly",
+    value: "readonly 속성이 붙은 읽기 전용 폼입니다"
+  },
+  required: {
+    label: "필수 입력 폼",
+    state: "required",
+    value: "필수 입력 폼은 라벨 오른쪽에 별이 붙습니다"
+  },
+  requiredError: {
+    label: "필수 입력 + 에러",
+    state: "required",
+    placeholder: "에러는 어떤 폼에든 쓸 수 있습니다",
+    hasError: "에러가 있으면 이렇게 색상이 바뀌어요!"
+  }
+}
 
+// 데모 생성 옵션
+const textInputDemo = [{
+    id: "demo1",
+    title: "Default",
+    elem: new TextInput(textInputOptions.normal)
+  },
+  {
+    id: "demo2",
+    title: "Required",
+    desc: "<code>state-required</code> 클래스를 추가합니다.",
+    elem: new TextInput(textInputOptions.required)
+  },
+  {
+    id: "demo4",
+    title: "Error & Required",
+    desc: "<code>state-error</code> 클래스와 <code>state-required</code> 클래스를 추가합니다.",
+    elem: new TextInput(textInputOptions.requiredError)
+  },
+  {
+    id: "demo3",
+    title: "Disabled",
+    desc: "input에 <code>disabled</code> 속성을 추가합니다.",
+    elem: new TextInput(textInputOptions.disabled)
+  },
+  {
+    id: "demo5",
+    title: "Readonly",
+    desc: "input에 <code>readonly</code> 속성을 추가합니다.",
+    elem: new TextInput(textInputOptions.readonly)
+  }
+]
 
-textInputGuide.appendChild(new DemoContainer(obj));
-textInputGuide.appendChild(new DemoContainer(obj2));
+// 텍스트 인풋 - 데모 컨테이너 생성
+for (let i = 0; i < textInputDemo.length; i++) {
+  textInputGuide.appendChild(new DemoContainer(textInputDemo[i]));
+}
