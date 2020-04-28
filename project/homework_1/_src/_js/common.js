c95c1c45b33eefd5b6940a86b9fe29060f6cb5d9
nana <nykim@nykim.net>
2020-04-28 18:28:37 +0900

    Update for transition motion (h1)

--- project/homework_1/_src/_js/common.js
+++ project/homework_1/_src/_js/common.js
@@ -12,11 +12,23 @@ const pageEvent = {
     body.classList.remove("-event-show");
   }
 }
+const detectSafari = () => {
+  var isSafari = navigator.vendor && navigator.vendor.indexOf('Apple') > -1 &&
+    navigator.userAgent &&
+    navigator.userAgent.indexOf('CriOS') == -1 &&
+    navigator.userAgent.indexOf('FxiOS') == -1;
+
+  if (isSafari) {
+    document.body.classList.add("-agent-safari");
+  }
+}
 
 const loadHandler = () => {
   document.body.classList.add("loaded");
   btnApply.addEventListener("click", pageEvent.open);
   btnClose.addEventListener("click", pageEvent.close);
+  detectSafari();
 }
+
 
 window.addEventListener("load", loadHandler);
